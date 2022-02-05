72cee3ae83c6360ac0de27e793cbcaf80f8f0835
ingodahn <ingodahn@gmail.com>
2022-02-05 17:19:52 +0100

    initial version

---
+++ imports/ui/components/saveHtml.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+        <v-btn color="primary" @click="saveHtml">Save as HTML</v-btn>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            head: `<!DOCTYPE html>
+            <html lang="en">
+            `,
+            html: ''
+        }
+    },
+    methods: {
+        saveHtml () {
+            this.html = this.$refs.editor.getHtml()
+            this.$refs.saveHtml.show()
+        }
+    }
+}
+</script>
